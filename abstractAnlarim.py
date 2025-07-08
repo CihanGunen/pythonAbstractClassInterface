157d5c806c274ce2e2527f3317e3b8b5d73e5ae0
Cihan Gunen <cihangunen7@gmail.com>
2025-07-08 14:21:11 +0300

    ChessPieces sinifindan türetilmiş bütün klaslar bu fonksiyonlari tanimlamak zorunda

--- abstractAnlarim.py
+++ abstractAnlarim.py
@@ -7,3 +7,31 @@ class ChessPieces(ABC):
     @abstractmethod 
     def show_count_in_board(self):
         pass
+
+
+class at(ChessPieces):
+    
+    def show_move(self):
+        pass 
+    
+    def show_count_in_board(self):
+        pass
+
+
+
+class fil(ChessPieces):
+    def show_move(self):
+        pass 
+    
+    def show_count_in_board(self):
+        pass
+
+
+
+class vezir(ChessPieces):
+    
+    def show_move(self):
+        pass 
+    
+    def show_count_in_board(self):
+        pass

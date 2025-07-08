6a125515b35f73fe6f337364468d93043efbdb16
Cihan Gunen <cihangunen7@gmail.com>
2025-07-08 14:15:21 +0300

    abstract class oluşturuldu

--- abstractAnlarim.py
+++ abstractAnlarim.py
@@ -1,1 +1,9 @@
 from abc import ABC, abstractmethod
+
+class ChessPieces(ABC):
+    @abstractmethod
+    def show_move(self):
+        pass
+    @abstractmethod 
+    def show_count_in_board(self):
+        pass
